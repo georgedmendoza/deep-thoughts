1f47769acccde40183e65da2f2aac7c5fc6c1c18
georgedmendoza <georgedmendoza13@gmail.com>
2021-11-03 18:43:02 -0500

    heroku

--- server/config/connection.js
+++ server/config/connection.js
@@ -4,9 +4,9 @@ mongoose.connect(
   process.env.MONGODB_URI || 'mongodb://localhost/deep-thoughts', 
   {
     useNewUrlParser: true,
-    useUnifiedTopology: true
-    // useCreateIndex: true,
-    // useFindAndModify: false
+    useUnifiedTopology: true,
+    useCreateIndex: true,
+    useFindAndModify: false
 });
 
 module.exports = mongoose.connection;
